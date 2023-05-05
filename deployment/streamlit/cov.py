87ca8aabfd1445bee0b2dcd59d24dc48889dd3f9
MohamedHamdy28 <m.abdelhamid@innopolis.university>
2023-05-05 11:32:40 +0300

    fixed deployment

--- deployment/streamlit/cov.py
+++ deployment/streamlit/cov.py
@@ -4,21 +4,72 @@ import json
 from matplotlib import image
 import requests
 from PIL import Image
+import numpy as np
+from skimage import transform
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout, BatchNormalization
+
+def create_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), strides=1, padding='same',
+                     activation='relu', input_shape=(150, 150, 1)))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D((2, 2), strides=2, padding='same'))
+    model.add(Conv2D(64, (3, 3), strides=1, padding='same', activation='relu'))
+    model.add(Dropout(0.1))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D((2, 2), strides=2, padding='same'))
+    model.add(Conv2D(64, (3, 3), strides=1, padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D((2, 2), strides=2, padding='same'))
+    model.add(Conv2D(128, (3, 3), strides=1, padding='same', activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D((2, 2), strides=2, padding='same'))
+    model.add(Conv2D(256, (3, 3), strides=1, padding='same', activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D((2, 2), strides=2, padding='same'))
+    model.add(Flatten())
+    model.add(Dense(units=128, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1, activation='sigmoid'))
+    model.compile(optimizer="rmsprop",
+                  loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+def preprocess(np_image):
+    np_image = Image.open(np_image)
+    np_image = np.array(np_image).astype('float32')/255
+    np_image = transform.resize(np_image, (150, 150, 1))
+    np_image = np.expand_dims(np_image, axis=0)
+    return np_image
 
 
-url = 'http://127.0.0.1:5001/api/'
+def get_prediction(img_path):
+    '''
+        This function takes as an input the image path
+        and return the model prediction for this image ['PNEUMONIA', 'NORMAL']
+        This model is trained on this dataset: https://www.kaggle.com/datasets/paultimothymooney/chest-xray-pneumonia
+    '''
+    img = preprocess(img_path)
+
+    prediction = model.predict(img)[0][0]
+
+    if prediction > 0.5:
+        return 'Normal'
+
+    return 'Pneumonia'
+
 
 st.write("Pneumonia x-ray classification")
 st.markdown("### Pneumonia recognition")
+model = create_model()
+model.load_weights(r'../model/weights.ckpt')
 
 # Create a file upload field
 uploaded_file = st.file_uploader(
     "Choose the x-ray scan image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    image_data = image.imread(uploaded_file)
-    j_data = json.dumps(image_data.tolist())
-
-    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-    r = requests.post(url, data=j_data, headers=headers)
-    st.write(r.text)
+    st.write(get_prediction(uploaded_file))
